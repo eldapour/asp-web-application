5e9e36e37675ff2a2c8eded43d05b4544bdf7e34
Abdallah Mahmoud <abdallahmahmoud1422003@gmail.com>
2023-10-19 20:25:28 +0300

    Add project files.

---
+++ WebApplication1/Controllers/testController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    public class testController:Controller
+    {
+       public ActionResult index()
+        {
+            //return "Welcome to Ya Abdallah";
+            return View("~/Views/index.cshtml");
+        }
+
+        public void create(string userName, string password)
+        {
+            //
+        }
+    }
+}
