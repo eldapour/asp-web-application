02baf1eaeb51694984a6cbe30541c637cc024e50
Abdallah Mahmoud <abdallahmahmoud1422003@gmail.com>
2023-11-07 21:06:35 +0200

    BaseController

---
+++ WebApplication1/Controllers/BaseController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    public class BaseController : Controller
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var act = context.RouteData.Values["action"].ToString();
+            var ctr = context.RouteData.Values["controller"].ToString();
+        }
+    }
+}
--- WebApplication1/Controllers/UserController.cs
+++ WebApplication1/Controllers/UserController.cs
@@ -8,7 +8,7 @@ using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
-    public class UserController : Controller
+    public class UserController : BaseController
     {
         private readonly IUserRepository userRepository;
 
